a4fd4c07cd0d0138281e6bfec409c3a5d2a04352
user pc <user pc@FATMAALBURIKI>
2026-08-30 13:31:35 +0400

    Add database script

---
+++ database-script.sql
@@ -0,0 +1,10 @@
+﻿
+CREATE TABLE Regions (Id INT IDENTITY PRIMARY KEY, Name NVARCHAR(MAX));
+INSERT INTO Regions (Name) VALUES ('Muscat'), ('Dhofar');
+CREATE TABLE Wilayas (Id INT IDENTITY PRIMARY KEY, Name NVARCHAR(MAX), RegionId INT);
+CREATE TABLE Areas (Id INT IDENTITY PRIMARY KEY, Name NVARCHAR(MAX), WilayaId INT);
+CREATE TABLE Villages (Id INT IDENTITY PRIMARY KEY, Name NVARCHAR(MAX), AreaId INT);
+
+INSERT INTO Wilayas (Name, RegionId) VALUES ('Muttrah', 1), ('Salalah', 2);
+INSERT INTO Areas (Name, WilayaId) VALUES ('Al Hamriya', 1);
+INSERT INTO Villages (Name, AreaId) VALUES ('Test Village', 1);
